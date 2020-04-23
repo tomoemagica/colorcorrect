a3960be244b57a93fe9b8bb8c0bfa531e35be07b
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-04-23 14:38:33 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
-from distutils.core import setup, Extension
+try: 
+    from setuptools import setup 
+    from setuptools import Extension 
+except ImportError: 
+    from distutils.core import setup 
+    from distutils.extension import Extension 
 import sys
 sys.path.append('./src')
 version = open('VERSION').read().strip()
@@ -9,30 +14,16 @@ libs = []
 if os.name == 'posix':
     libs.append('m')
 
-module1 = Extension('colorcorrect._cutil',
-                    include_dirs=['cutil'],
-                    libraries=libs,
-                    sources = ['cutil/cutil.cpp'])
-
 setup(name='colorcorrect',
       version=version,
       description="imprement some of color correction algorithms",
       long_description=open('README').read(),
       classifiers=[],
       keywords=('image-processing computer-vision'),
-      install_requires=["numpy", "Pillow", "six"],
       author='Tomoe Magica',
       author_email='tomoe.magica@gmail.com',
       url='https://github.com/tomoemagica/colorcorrect',
       license='MIT License',
       package_dir={'': 'src'},
-      packages=['colorcorrect'],
-      ext_modules = [module1])
-#         extra_compile_args=[],
-#         Extension(
-#             'colorcorrect._cutil', [
-#                 'cutil/cutil.cpp',
-#             ],
-#         ),
-#     ],
-#     )
+      packages=['colorcorrect']
+)
