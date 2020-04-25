90ecfc6d7f2689886f3d5c088f8178689181a6e3
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-04-25 16:29:14 +0900

    Add files via upload

--- test/demo.py
+++ test/demo.py
@@ -3,6 +3,6 @@ import colorcorrect.algorithm as cca
 from colorcorrect.util import from_pil, to_pil
 
 img = Image.open('demo.jpg')
-to_pil(cca.stretch(cca.gray_world(from_pil(img)))).show()
-img = to_pil(cca.stretch(cca.gray_world(from_pil(img))))
+# img = to_pil(cca.stretch(cca.gray_world(from_pil(img))))
+img = to_pil(cca.automatic_color_equalization(from_pil(img)))
 img.save("result.jpg", quality=100)
