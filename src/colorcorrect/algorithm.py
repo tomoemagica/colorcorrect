c8d0488672cd1b701fc5131c1139b1e6fec48fee
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-05-10 16:13:43 +0900

    Update algorithm.py

--- src/colorcorrect/algorithm.py
+++ src/colorcorrect/algorithm.py
@@ -1,38 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-# import numpy as np
-import cupy as np
-import ctypes.util
-import ctypes
-from ctypes import POINTER
-from ctypes import pointer
-from ctypes import Structure
-from ctypes import c_ubyte
-from ctypes import c_int
-from ctypes import c_double
-from ctypes import c_void_p
-from ctypes import *
-from six.moves import range
-from six.moves import xrange
-import cv2
-
-import os
-cutilfolder = os.path.abspath(__file__).rsplit(os.path.sep, 1)[0]
-cutilname = "cutil"
-libcutil = np.ctypeslib.load_library(cutilname, cutilfolder)
-
-
-class RGBImage(Structure):
-    _fields_ = [
-        ("width", c_int),
-        ("height", c_int),
-        ("r", POINTER(c_ubyte)),
-        ("g", POINTER(c_ubyte)),
-        ("b", POINTER(c_ubyte)),
-    ]
-
-
-libcutil.calc_ace.argtypes = [POINTER(RGBImage), c_int, c_double, c_double]
+import numpy as np
 
 
 def stretch_pre(nimg):
@@ -74,36 +42,3 @@ def max_white(nimg):
 def stretch(nimg):
     return max_white(stretch_pre(nimg))
 
-
-def automatic_color_equalization(nimg, slope=10, limit=1000, samples=500):
-    """
-    A. Rizzi, C. Gatta and D. Marini, "A new algorithm for unsupervised global and local color correction.",
-    Pattern Recognition Letters, vol. 24, no. 11, 2003.
-    """
-    nimg = nimg.transpose(2, 0, 1)
-    nimg = np.ascontiguousarray(nimg, dtype=np.uint8)
-    img = RGBImage(nimg.shape[2],
-                   nimg.shape[1],
-                   nimg[0].ctypes.data_as(POINTER(c_ubyte)),
-                   nimg[1].ctypes.data_as(POINTER(c_ubyte)),
-                   nimg[2].ctypes.data_as(POINTER(c_ubyte)))
-    libcutil.calc_ace(pointer(img), samples, slope, limit)
-    return nimg.transpose(1, 2, 0)
-
-
-def clahe(img):
-    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(2,2))
-    img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0])
-    nimg = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-    return nimg
-
-
-def white_balance(img):
-    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    avg_a = np.average(result[:, :, 1])
-    avg_b = np.average(result[:, :, 2])
-    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
-    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
-    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
-    return result
